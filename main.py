a6e06ba5e40e24adb4fde289d2849ca50426e9a3
Gabriela Pineda <gabimartinez883@gmail.com>
2023-08-08 21:40:10 -0600

    kata resuelto

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 import unittest
 
 
-def sum(a, b):
-    return a + b
 
-
-class SumTest(unittest.TestCase):
-    def test_sum(self):
-        self.assertEqual(sum(1, 2), 5)
-
-
-if __name__ == '__main__':
-    unittest.main()
+class GreeterTest(unittest.TestCase):
+    test_name = "Gabriela"
+    expected_greet = f"hola" 
+    
