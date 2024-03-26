a46e36f5709fcbc567e98d31707729c7c59fd380
Mi Primer Programa <gabimartinez883@gmail.com>
2024-03-26 17:05:59 -0600

    unitest

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import unittest
 
-
 def sum(a, b):
     return a + b
 
-
 class SumTest(unittest.TestCase):
     def test_sum(self):
-        self.assertEqual(sum(1, 2), 5)
-
+        self.assertEqual(sum(1, 2), 3)
 
 if __name__ == '__main__':
     unittest.main()
